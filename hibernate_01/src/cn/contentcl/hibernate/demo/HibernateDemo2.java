4d29ebc7f284c6a25abb5e615bbd4e78856be207
changjiale <1056430312@qq.com>
2018-12-08 16:06:23 +0800

    hibernate更新删除

--- hibernate_01/src/cn/contentcl/hibernate/demo/HibernateDemo2.java
+++ hibernate_01/src/cn/contentcl/hibernate/demo/HibernateDemo2.java
@@ -1,5 +1,10 @@
 package cn.contentcl.hibernate.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.Test;
@@ -53,6 +58,91 @@ public class HibernateDemo2 {
 		Customer customer = session.load(Customer.class, 2l);
 		System.out.println(customer);
 		
+		
+		tx.commit();
+		session.close();
+	}
+	
+	@Test
+	//修改操作
+	public void demo3() {
+		Session session = HibernateUtils.openSession();
+		Transaction tx = session.beginTransaction();
+		/*//直接创建对象，进行修改
+		Customer  customer = new Customer();
+		customer.setCust_id(1l);
+		customer.setCust_name("王小儿");
+		session.update(customer);*/
+		
+		//先查询，再修改（推荐）
+		Customer  customer = session.get(Customer.class,2l);
+		customer.setCust_name("王王");
+		session.update(customer);
+		tx.commit();
+		session.close();
+	}
+	
+	@Test
+	//删除操作
+	public void demo4() {
+		Session session = HibernateUtils.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		//直接删除对象，删除
+		/*Customer customer = new Customer();
+		customer.setCust_id(1l);
+		session.delete(customer);*/
+		
+		//先查询在删除（推荐）--级联删除
+		Customer customer = session.get(Customer.class, 3l);
+		session.delete(customer);
+		
+		
+		tx.commit();
+		session.close();
+	}
+	
+	@Test
+	//保存或更新
+	public void demo5() {
+		Session session = HibernateUtils.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		
+//		Customer customer = new Customer();
+//		customer.setCust_name("王兰");
+//		session.saveOrUpdate(customer);
+		
+		Customer customer = new Customer();
+		customer.setCust_id(5l);
+		customer.setCust_name("王x兰");
+		session.saveOrUpdate(customer);
+		
+		
+		
+		tx.commit();
+		session.close();
+	}
+	
+	@Test
+	//保存或更新
+	public void demo6() {
+		Session session = HibernateUtils.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		//接受HQL：Hibernate Query Language 面向对象的查询语言
+		Query query = session.createQuery("from Customer");
+		List<Customer> list = query.list();
+		for (Customer customer : list) {
+			System.out.println(customer);
+		}
+		
+		//接收sql语句
+		SQLQuery query1 = session.createSQLQuery("select * from cst_customer");
+		List<Object[]> list1 = query1.list();
+		for (Object[] objects : list1) {
+			System.out.println(Arrays.toString(objects));
+		}
 		
 		tx.commit();
 		session.close();
