b6b16954b43d46a82e334fdcafb71522cefa279f
changjiale <1056430312@qq.com>
2018-12-10 21:26:06 +0800

    建立用户表和联系人表并生成方法

---
+++ hibernate_03/src/cn/contentcl/hibernate/domain/Customer.java
@@ -0,0 +1,71 @@
+package cn.contentcl.hibernate.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 客户的实体
+ * @author leer
+ *
+ */
+public class Customer {
+	private Long cust_id;
+	private String cust_name;
+	private String cust_source;
+	private String cust_industry;
+	private String cust_level;
+	private String cust_phone;
+	private String cust_mobile;
+	//通过ORM方式表示：一个客户对应多个联系人
+	//放置的多的一方的集合。Hibernate默认使用Set集合
+	private Set<LinkMan> linkmans = new HashSet<LinkMan>();
+	
+	public Set<LinkMan> getLinkmans() {
+		return linkmans;
+	}
+	public void setLinkmans(Set<LinkMan> linkmans) {
+		this.linkmans = linkmans;
+	}
+	public Long getCust_id() {
+		return cust_id;
+	}
+	public void setCust_id(Long cust_id) {
+		this.cust_id = cust_id;
+	}
+	public String getCust_name() {
+		return cust_name;
+	}
+	public void setCust_name(String cust_name) {
+		this.cust_name = cust_name;
+	}
+	public String getCust_source() {
+		return cust_source;
+	}
+	public void setCust_source(String cust_source) {
+		this.cust_source = cust_source;
+	}
+	public String getCust_industry() {
+		return cust_industry;
+	}
+	public void setCust_industry(String cust_industry) {
+		this.cust_industry = cust_industry;
+	}
+	public String getCust_level() {
+		return cust_level;
+	}
+	public void setCust_level(String cust_level) {
+		this.cust_level = cust_level;
+	}
+	public String getCust_phone() {
+		return cust_phone;
+	}
+	public void setCust_phone(String cust_phone) {
+		this.cust_phone = cust_phone;
+	}
+	public String getCust_mobile() {
+		return cust_mobile;
+	}
+	public void setCust_mobile(String cust_mobile) {
+		this.cust_mobile = cust_mobile;
+	}
+}
