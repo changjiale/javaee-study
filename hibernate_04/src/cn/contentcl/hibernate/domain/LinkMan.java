8279db6d695426381f1d6b67e90e15d83f2fe8bb
changjiale <1056430312@qq.com>
2018-12-13 20:55:48 +0800

    hibernate HQL查询

---
+++ hibernate_04/src/cn/contentcl/hibernate/domain/LinkMan.java
@@ -0,0 +1,90 @@
+package cn.contentcl.hibernate.domain;
+
+/**
+ * 联系人实体
+ * @author leer
+ *
+ */
+public class LinkMan {
+	private Long lkm_id;
+	private String lkm_name;
+	private String lkm_gender;
+	private String lkm_phone;
+	private String lkm_mobile;
+	private String lkm_email;
+	private String lkm_qq;
+	private String lkm_position;
+	private String lkm_memo;
+	//通过ORM方式表示：一个联系人之恶能属于某一个客户。
+	//放置的是一的一方的对象
+	private Customer customer;
+	public Long getLkm_id() {
+		return lkm_id;
+	}
+	public void setLkm_id(Long lkm_id) {
+		this.lkm_id = lkm_id;
+	}
+	public String getLkm_name() {
+		return lkm_name;
+	}
+	public void setLkm_name(String lkm_name) {
+		this.lkm_name = lkm_name;
+	}
+	public String getLkm_gender() {
+		return lkm_gender;
+	}
+	public void setLkm_gender(String lkm_gender) {
+		this.lkm_gender = lkm_gender;
+	}
+	public String getLkm_phone() {
+		return lkm_phone;
+	}
+	public void setLkm_phone(String lkm_phone) {
+		this.lkm_phone = lkm_phone;
+	}
+	public String getLkm_mobile() {
+		return lkm_mobile;
+	}
+	public void setLkm_mobile(String lkm_mobile) {
+		this.lkm_mobile = lkm_mobile;
+	}
+	public String getLkm_email() {
+		return lkm_email;
+	}
+	public void setLkm_email(String lkm_email) {
+		this.lkm_email = lkm_email;
+	}
+	public String getLkm_qq() {
+		return lkm_qq;
+	}
+	public void setLkm_qq(String lkm_qq) {
+		this.lkm_qq = lkm_qq;
+	}
+	public String getLkm_position() {
+		return lkm_position;
+	}
+	public void setLkm_position(String lkm_position) {
+		this.lkm_position = lkm_position;
+	}
+	public String getLkm_memo() {
+		return lkm_memo;
+	}
+	public void setLkm_memo(String lkm_memo) {
+		this.lkm_memo = lkm_memo;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	@Override
+	public String toString() {
+		return "LinkMan [lkm_id=" + lkm_id + ", lkm_name=" + lkm_name + ", lkm_gender=" + lkm_gender + ", lkm_phone="
+				+ lkm_phone + ", lkm_mobile=" + lkm_mobile + ", lkm_email=" + lkm_email + ", lkm_qq=" + lkm_qq
+				+ ", lkm_position=" + lkm_position + ", lkm_memo=" + lkm_memo + "]";
+	}
+	
+	
+	
+}
