288201fbd574004e4586e90aeb13a91926631438
changjiale <1056430312@qq.com>
2019-01-28 20:14:26 +0800

    声明式事务二：注解开发

---
+++ spring4_day03_jdbc/src/live/leer/tx/demo3/AccountServiceImpl.java
@@ -0,0 +1,36 @@
+package live.leer.tx.demo3;
+
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Transactional(isolation=Isolation.DEFAULT,propagation=Propagation.REQUIRED)
+public class AccountServiceImpl implements AccountService {
+	//注入DAO(xml方式注入)
+	private AccountDao accountDao;
+	
+	public void setAccountDao(AccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+	
+
+	/**
+	 * from 转出账号
+	 * to  转入账号
+	 * money 转账金额
+	 */
+	@Override
+	public void transfer(String from, String to, Double money) {
+		// TODO Auto-generated method stub
+		
+		accountDao.outMoney(from, money);
+		int i = 1/0;
+		accountDao.inMoney(to, money);
+
+		
+	}
+
+}
---
+++ spring4_day03_jdbc/src/live/leer/tx/demo3/SpringDemo1.java
@@ -0,0 +1,26 @@
+package live.leer.tx.demo3;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 测试转账环境
+ * @author leer
+ *
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:tx3.xml")
+public class SpringDemo1 {
+	@Resource(name="accountService")
+	private AccountService accountService;
+	
+	@Test
+	public void demo1() {
+		accountService.transfer("王宝强", "王五", 100d);
+	}
+}
