d080334e69dd7626c1fc01800a425b6af6146545
changjiale <1056430312@qq.com>
2019-01-28 20:03:39 +0800

    声明式事务一：xml方式

---
+++ spring4_day03_jdbc/src/live/leer/tx/demo2/AccountService.java
@@ -0,0 +1,5 @@
+package live.leer.tx.demo2;
+
+public interface AccountService {
+	public void transfer(String from, String to, Double money);
+}
---
+++ spring4_day03_jdbc/src/live/leer/tx/demo2/AccountServiceImpl.java
@@ -0,0 +1,32 @@
+package live.leer.tx.demo2;
+
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+public class AccountServiceImpl implements AccountService {
+	//注入DAO(xml方式注入)
+	private AccountDao accountDao;
+	
+	public void setAccountDao(AccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+	
+
+	/**
+	 * from 转出账号
+	 * to  转入账号
+	 * money 转账金额
+	 */
+	@Override
+	public void transfer(String from, String to, Double money) {
+		// TODO Auto-generated method stub
+		
+		accountDao.outMoney(from, money);
+		// int i = 1/0;
+		accountDao.inMoney(to, money);
+
+		
+	}
+
+}
