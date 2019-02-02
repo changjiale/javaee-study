2e95ed09eea9f87550a87eff0234cc3a971fae1e
changjiale <1056430312@qq.com>
2019-02-02 17:20:50 +0800

    maven构建web项目

---
+++ helloWord/src/main/java/live/leer/web/HelloServlet.java
@@ -0,0 +1,26 @@
+package live.leer.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("进入HelloServlet");
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
