8b3a204dcab646211dde698b488cab71857204b4
changjiale <1056430312@qq.com>
2019-02-07 14:39:59 +0800

    maven整合struts，hibernate

---
+++ mavenstruts/src/main/java/live/leer/web/action/HelloAction.java
@@ -0,0 +1,13 @@
+package live.leer.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("进入helloAction 返回默认值");
+		return SUCCESS;
+	}
+}
