977021186dd0d6b2723899d7dc33907385074a40
changjiale <1056430312@qq.com>
2019-02-17 15:49:29 +0800

    sqlmap生成器自动创建映射

---
+++ mybatis03-spring/src/live/leer/mybatis/test/UserMapperTestG.java
@@ -0,0 +1,39 @@
+package live.leer.mybatis.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserMapperTestG {
+
+private ApplicationContext applicationContext;
+	
+	@Before
+	public void init(){
+		applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+
+	@Test
+	public void testInsertSelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByExample() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKeySelective() {
+		fail("Not yet implemented");
+	}
+
+}
