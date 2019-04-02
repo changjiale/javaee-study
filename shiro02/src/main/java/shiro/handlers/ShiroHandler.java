0127c7f523748f32b45f1c2573175e81b354a0c3
changjiale <1056430312@qq.com>
2019-04-02 18:43:40 +0800

    单realm-md5盐值加密

---
+++ shiro02/src/main/java/shiro/handlers/ShiroHandler.java
@@ -0,0 +1,39 @@
+package shiro.handlers;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/shiro")
+public class ShiroHandler {
+
+    @RequestMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password){
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            // 把用户名和密码封装为 UsernamePasswordToken 对象
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            // rememberme
+            token.setRememberMe(true);
+            try {
+                System.out.println("1. " + token.hashCode());
+                // 执行登录.
+                currentUser.login(token);
+            }
+            // ... catch more exceptions here (maybe custom ones specific to your application?
+            // 所有认证时异常的父类.
+            catch (AuthenticationException ae) {
+                //unexpected condition?  error?
+                System.out.println("登录失败: " + ae.getMessage());
+            }
+        }
+
+        return "redirect:/list.jsp";
+    }
+}
