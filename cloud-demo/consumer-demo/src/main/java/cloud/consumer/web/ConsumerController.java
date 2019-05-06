ac141cfe2870c62db562e675daf5241f91baa023
changjiale <1056430312@qq.com>
2019-05-06 20:57:46 +0800

    eureka高可用集群

--- cloud-demo/consumer-demo/src/main/java/cloud/consumer/web/ConsumerController.java
+++ cloud-demo/consumer-demo/src/main/java/cloud/consumer/web/ConsumerController.java
@@ -3,17 +3,30 @@ package cloud.consumer.web;
 
 import cloud.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("consumer")
 public class ConsumerController{
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+
     @GetMapping("{id}")
     public User queryById(@PathVariable("id") Long id) {
-        String url = "http://localhost:8081/user/"+id;
+        //根据服务id获取实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        //从实例中取出ip和端口
+        ServiceInstance instance = instances.get(0);
+        //随机， 轮询， hash，最小访问
+        String url = "http://"+instance.getHost()+ ":" + instance.getPort()+"/user/"+id;
         User user = restTemplate.getForObject(url, User.class);
         return user;
     }
--- cloud-demo/user-service/src/main/java/cloud/UserApplication.java
+++ cloud-demo/user-service/src/main/java/cloud/UserApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("cloud.user.mapper")
 public class UserApplication {
