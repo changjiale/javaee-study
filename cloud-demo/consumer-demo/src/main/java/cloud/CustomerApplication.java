68e541766a8ce9404b878900d82c05e242143851
changjiale <1056430312@qq.com>
2019-05-07 15:31:50 +0800

    cloud 负载均衡机制

--- cloud-demo/consumer-demo/src/main/java/cloud/CustomerApplication.java
+++ cloud-demo/consumer-demo/src/main/java/cloud/CustomerApplication.java
@@ -3,6 +3,7 @@ package cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class CustomerApplication {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- cloud-demo/consumer-demo/src/main/java/cloud/consumer/web/ConsumerController.java
+++ cloud-demo/consumer-demo/src/main/java/cloud/consumer/web/ConsumerController.java
@@ -5,6 +5,7 @@ import cloud.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,18 +16,21 @@ import java.util.List;
 public class ConsumerController{
     @Autowired
     private RestTemplate restTemplate;
-    @Autowired
-    private DiscoveryClient discoveryClient;
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//    @Autowired
+//    private RibbonLoadBalancerClient client;
 
 
     @GetMapping("{id}")
     public User queryById(@PathVariable("id") Long id) {
-        //根据服务id获取实例
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
-        //从实例中取出ip和端口
-        ServiceInstance instance = instances.get(0);
+//        //根据服务id获取实例
+//        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+//        //从实例中取出ip和端口
+//        ServiceInstance instance = instances.get(0);
+//        ServiceInstance instance = client.choose("user-service");
         //随机， 轮询， hash，最小访问
-        String url = "http://"+instance.getHost()+ ":" + instance.getPort()+"/user/"+id;
+        String url = "http://user-service/user/"+id;
         User user = restTemplate.getForObject(url, User.class);
         return user;
     }
