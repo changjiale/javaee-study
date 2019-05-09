1f9fb23b52bfcdc762770accee6f16bc4912d0ac
changjiale <1056430312@qq.com>
2019-05-09 17:03:39 +0800

    zuul网关 登陆拦截和路由

--- cloud-demo/consumer-demo/src/main/java/cloud/consumer/web/ConsumerController.java
+++ cloud-demo/consumer-demo/src/main/java/cloud/consumer/web/ConsumerController.java
@@ -16,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
-@RequestMapping("consumer")
+@RequestMapping("/consumer")
 //熔断返回函数
 //@DefaultProperties(defaultFallback = "queryByIdFallBack")
 public class ConsumerController{
---
+++ cloud-demo/gateway/src/main/java/cloud/filter/LoginFilter.java
@@ -0,0 +1,47 @@
+package cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        //获取请求上下文
+        RequestContext ctx = RequestContext.getCurrentContext();
+        //获取request
+        HttpServletRequest request = ctx.getRequest();
+
+        //获取请求参数 access_token
+        String token = request.getParameter("access_token");
+        //判断是否存在
+        if (StringUtils.isAllBlank(token)) {
+            //不存在，未登录，则拦截
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(HttpStatus.FORBIDDEN.value());
+        }
+        return null;
+    }
+}
